1b517946fbfce53329ce93c3f5660a9d936be57e
kamal garg <kamal.garg1024@gmail.com>
2023-08-04 22:00:51 +0530

    Refactor: simplify game query

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
-  genre: Genre | null;
-  platform: Platform | null;
+  genreId?: number;
+  platformId?: number;
   sortOrder: string;
   searchText: string;
 }
@@ -31,18 +31,18 @@ function App() {
       }}
     >
       <GridItem area={"nav"}>
-        {" "}
         <NavBar
           onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
-        />{" "}
+        />
       </GridItem>
       <Show above="lg">
         <GridItem area={"aside"} paddingX={5}>
-          {" "}
           <GenreList
-            selectedGenre={gameQuery.genre}
-            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
-          />{" "}
+            selectedGenreId={gameQuery.genreId}
+            onSelectGenre={(genre) =>
+              setGameQuery({ ...gameQuery, genreId: genre.id })
+            }
+          />
         </GridItem>
       </Show>
       <GridItem area={"main"}>
@@ -51,9 +51,9 @@ function App() {
           <Flex marginBottom={5}>
             <Box marginRight={5}>
               <PlatformSelector
-                selectedPlatform={gameQuery.platform}
+                selectedPlatformId={gameQuery.platformId}
                 onSelectPlatform={(platform) =>
-                  setGameQuery({ ...gameQuery, platform })
+                  setGameQuery({ ...gameQuery, platformId: platform.id })
                 }
               />
             </Box>
